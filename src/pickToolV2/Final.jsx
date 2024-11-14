517ba1cd29250130db0f92aad269313b0666501f
KangJS <asdfqaz1996@naver.com>
2024-11-14 20:05:46 +0900

    [Feature] Final 컴포넌트에 이전 단계로 이동하는 버튼 추가 및 진행도 상태 관리 구현

--- src/pickToolV2/Final.jsx
+++ src/pickToolV2/Final.jsx
@@ -1,3 +1,16 @@
+import { progressAtom } from "atoms/userAtoms";
+import { useAtom } from "jotai";
+
 export const Final = () => {
-  return <div>Final</div>;
+  const [, setStep] = useAtom(progressAtom); // 진행도
+
+  const handlePrev = () => {
+    setStep(2);
+  };
+  return (
+    <>
+      <div>Final</div>
+      <button onClick={handlePrev}>이전으로</button>
+    </>
+  );
 };
